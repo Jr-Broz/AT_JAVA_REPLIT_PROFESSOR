e6f93faed98a1f9cba1fe01212111662dfc04e0b
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-10-06 17:10:41 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+    Batalha jogar = new Batalha();
+    DadosBatalha mostraDado = new DadosBatalha();
+    
+    jogar.Batalhar();
+    mostraDado.mostraCoisa();
+
+  //Fora do replit, ele gera um arquivo .csv na pasta %temp% , Caso sr queira checar.
+
+  //Professor esse sistema de aleatorizar inimigo causa que algumas vezes nao apareça,     mas é só rodar o codigo dnv.
+  }
+}
