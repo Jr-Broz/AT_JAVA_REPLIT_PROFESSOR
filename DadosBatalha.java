47416a41c791d75b0afd067732f1b81177270f0b
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-10-06 17:11:34 -0300

    Create DadosBatalha.java

---
+++ DadosBatalha.java
@@ -0,0 +1,11 @@
+import java.util.List;
+public class DadosBatalha extends Batalha{
+  public void mostraCoisa(){
+
+    System.out.println("-----------------------------------------");
+
+    String resultado = String.join("; ", mArrayList);
+    System.out.println(resultado);
+ 
+  }
+}
